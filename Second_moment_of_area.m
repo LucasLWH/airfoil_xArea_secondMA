b4b5545cfb7b26bb721de2c8575f5c28b3351227
Lucas Lim <50956189+LucasLWH@users.noreply.github.com>
2020-12-06 01:56:56 +0800

    Update Second_moment_of_area.m
    Generalised form with comments to explain process

--- Second_moment_of_area.m
+++ Second_moment_of_area.m
@@ -2,32 +2,30 @@
 Written by Lucas Lim
 12/3/20
 %}
-%% Outer
-data = readmatrix('Generated_coord_Yardley.xlsx','Range','d1:e400');
-%% Inner
-% data = readmatrix('Generated_inner_Yardley.xlsx','Range','h1:i400');
+%% Import
+data = readmatrix('.xlsx','Range','');
+
 %% Process
-pgon = polyshape(data(:,1),data(:,2))
-
-% plot(pgon)
-hold on
+%Gen Polyshape
+pgon = polyshape(data(:,1),data(:,2));
+%Detect centroid & center on origin
 [cenX,cenY]=centroid(pgon)
-
-%Apply proper translations
 xShifted = data(:,1)-cenX;
 yShifted = data(:,2)-cenY;
 
+%Create centred polyshape
 pgon2 = polyshape(xShifted,yShifted);
 plot(pgon2)
 plot(zeros(length(xShifted)),yShifted,'r')
 plot(xShifted,zeros(length(yShifted)),'r')
 
-I_x = 0
+%Numerical Integration for 2nd moment of area
+I_x = 0;
 for i = 1:length(xShifted)-1
     U_n=(xShifted(i)*yShifted(i+1)-xShifted(i+1)*yShifted(i))*(yShifted(i)^2+yShifted(i)*yShifted(i+1)+yShifted(i+1)^2);
     I_x = I_x + (1/12)*U_n;
 end
-
 disp(I_x)
 
-Inner_area=area(pgon2)
+%Cross-sectional area of polyshape
+Cross_area=area(pgon2)
